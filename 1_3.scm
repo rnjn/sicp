65b29f2a18edbc87b54a27ff0625d163a5cba3b5
Ranjan Sakalley <rsakalle@thoughtworks.com>
2010-10-18 13:39:18 +0530

    First commit

---
+++ 1_3.scm
@@ -0,0 +1,23 @@
+(load "common.scm")
+
+(define (sum_of_squares x y) (+ (* x x) (* y y)))
+
+(define (greater x y) 
+  (cond ((> x y) x) 
+	(else y)))
+
+(define (lesser x y) 
+  (cond ((< x y) x) 
+	(else y)))
+
+
+ (define (sos_of_greater_pair x y z) ( 
+				      sum_of_squares (greater x y) (greater (lesser x y) z)))
+
+(assert-equals (greater 3 4) 4)
+(assert-equals (lesser 3 4) 3)
+
+(assert-equals (sos_of_greater_pair 1 3 4) 25)
+(assert-equals (sos_of_greater_pair -1 -3 -4) 10)
+
+
