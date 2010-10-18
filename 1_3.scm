6ee4d4b4c8be7b7155af867e068a76198d79e4f5
Ranjan Sakalley <rsakalle@thoughtworks.com>
2010-10-18 13:44:39 +0530

    added readme

--- 1_3.scm
+++ 1_3.scm
@@ -1,5 +1,14 @@
 (load "common.scm")
 
+;tests
+(assert-equals (greater 3 4) 4)
+
+(assert-equals (lesser 3 4) 3)
+
+(assert-equals (sos_of_greater_pair 1 3 4) 25)
+
+(assert-equals (sos_of_greater_pair -1 -3 -4) 10)
+
 (define (sum_of_squares x y) (+ (* x x) (* y y)))
 
 (define (greater x y) 
@@ -10,14 +19,6 @@
   (cond ((< x y) x) 
 	(else y)))
 
-
- (define (sos_of_greater_pair x y z) ( 
-				      sum_of_squares (greater x y) (greater (lesser x y) z)))
-
-(assert-equals (greater 3 4) 4)
-(assert-equals (lesser 3 4) 3)
-
-(assert-equals (sos_of_greater_pair 1 3 4) 25)
-(assert-equals (sos_of_greater_pair -1 -3 -4) 10)
-
+(define (sos_of_greater_pair x y z) ( 
+				     sum_of_squares (greater x y) (greater (lesser x y) z)))
 
