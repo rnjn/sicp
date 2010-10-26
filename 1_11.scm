f3d4c6bd3b359a8c4054e82010da0a71ca2ee79b
Ranjan Sakalley <rsakalle@thoughtworks.com>
2010-10-27 00:36:11 +0530

    fixed errors

--- 1_11.scm
+++ 1_11.scm
@@ -2,15 +2,15 @@
 
 ;recursive
 (define (f n)(
-	      cond ((<= n 3) n)
+	      cond ((< n 3) n)
 		   (else (+ (f (- n 1)) (* 2 (f (- n 2))) (* 3 (f (- n 3))))))) 
 
 		
 (assert-equals 2 (f 2))
-(assert-equals 10 (f 4))
-(assert-equals 22 (f 5))
-(assert-equals 51 (f 6))
-(assert-equals 1657 (f 10))
+(assert-equals 11 (f 4))
+(assert-equals 25 (f 5))
+(assert-equals 59 (f 6))
+(assert-equals 1892 (f 10))
 
 ;iterative 
 ;remember the last 2 f's and create the latest one. build till n
@@ -18,14 +18,14 @@
 		  (cond ((= i n) a)
 			(else (f-iter n (+ i 1) (+ a (* 2 b) (* 3 c)) a b))))
 
-; start from 3 and build till n		
+; start from 2 and build till n		
 (define (fi n) 
   (cond ((< n 3) n)
-		(else (f-iter n 3 3 2 1))))
+		(else (f-iter n 2 2 1 0))))
 		
 (assert-equals 2 (fi 2))
-(assert-equals 10 (fi 4))
-(assert-equals 22 (fi 5))
-(assert-equals 51 (fi 6))
-(assert-equals 1657 (fi 10))
+(assert-equals 11 (fi 4))
+(assert-equals 25 (fi 5))
+(assert-equals 59 (fi 6))
+(assert-equals 1892 (fi 10))
 		
