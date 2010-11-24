a9dc802f8ddda4f5f070e2b5e7cedaf242c910f4
Ranjan Sakalley <gogo@rnjn.corporate.thoughtworks.com>
2010-11-24 17:45:45 +0530

    prime numbers

---
+++ 1_21.scm
@@ -0,0 +1,19 @@
+(load "common.scm")
+
+;i = test divisor, which is initially 2
+(define (find-divisor n i)
+  (cond ((> (square i) n) n)
+	((= (remainder n i) 0) i)
+	(else
+	 (find-divisor n (+ i 1)))))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (prime? n)
+  (= (smallest-divisor n) n ))
+
+
+(assert-equals 199 (smallest-divisor 199))
+(assert-equals 1999 (smallest-divisor 1999))
+(assert-equals 7 (smallest-divisor 19999))
---
+++ 1_23.scm
@@ -0,0 +1,41 @@
+(load "common.scm")
+
+(define (next i)
+  (cond ((= i 2) 3)
+	(else
+	 (+ i 2))))
+  
+(define (find-divisor n i)
+  (cond ((> (square i) n) n)
+	((= (remainder n i) 0) i)
+	(else
+	 (find-divisor n (next i)))))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (prime? n)
+  (= (smallest-divisor n) n ))
+
+
+(define (find-prime n p start-time)
+  (cond ((prime? n)
+	 (newline)
+	 (display n) 
+	 (display " -> time taken : ")
+	 (display (- (runtime) start-time))
+	 (- p 1))
+	(else p)))
+
+
+(define (search-primes r1 r2 n)
+  (cond ((>= (+ r1 2) r2) n)
+	((= 0 n) n)
+	(else 
+	 (search-primes (+ r1 2) r2 (find-prime r1 n (runtime))))))
+	
+(search-primes 1001 2000 3)
+(search-primes 100000001 200000000 3)
+(search-primes 1000000001 2000000000 3)
+(search-primes 10000000001 20000000000 3)
+
