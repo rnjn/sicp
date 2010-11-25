6687ec183426b866488b86610ef50e6e8cee3905
rnjn <rsakalle@thoughtworks.com>
2010-11-25 13:27:07 +0530

    removed extra local variable

--- 1_30.scm
+++ 1_30.scm
@@ -8,17 +8,17 @@
 
 
 (define (summation-iterative n1 n2 f next)
-  (define (isummation n1 n2 state)
-    (cond ((> n1 n2) state)
+  (define (isummation n state)
+    (cond ((> n n2) state)
 	  (else
-	   (isummation (next n1) n2 (+ state (f n1))))))
-  (isummation n1 n2 0))
+	   (isummation (next n) (+ state (f n))))))
+  (isummation n1 0))
 
 (define (identity i) i)
 (define (inc i) (+ i 1))
 
-(define (sum-of-numbers n1 n2 method)
-  (method n1 n2 identity inc))
+(define (sum-of-numbers n1 n2 summation-method)
+  (summation-method n1 n2 identity inc))
 
 (assert-equals (sum-of-numbers 1 10 summation) 
 	       (sum-of-numbers 1 10 summation-iterative))
