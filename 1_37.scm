5eff2709273bada1bca89dea252010fd83847fb5
rnjn <rsakalle@thoughtworks.com>
2010-11-29 16:14:05 +0530

    golden ratio - number of iterations

--- 1_37.scm
+++ 1_37.scm
@@ -1,12 +1,20 @@
 (load "common.scm")
 
 (define (cont-frac n d k)
-  (define phi 1.6180)
   (define (next-term i)
     (cond ((= i k) 0) 
 	  (else
 	   (/ (n i) ( + (d i) (next-term (+ i 1)))))))
   (next-term 1))
 
+(define (min-k)
+  (define golden-ratio 1.6180)  
+  (define (good-enough? x) (< (abs (- golden-ratio x)) 0.0001))
+  (define (iterate k)
+    (let ((phi (/ 1 (cont-frac (lambda (i) 1.0) (lambda (i) 1.0) k))))
+      (cond ((good-enough? phi) k)
+	    (else
+	     (iterate (+ k 1))))))
+    (iterate 2))
 
-(/ 1 (cont-frac (lambda (i) 1.0) (lambda (i) 1.0) 1000))
+(min-k)
