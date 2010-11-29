762e9cf016c41fd1f15204c58818d016e222c13b
rnjn <rsakalle@thoughtworks.com>
2010-11-29 22:21:50 +0530

    double

---
+++ 1_41.scm
@@ -0,0 +1,11 @@
+(load "common.scm")
+
+(define (inc i) (+ i 1))
+
+(define (double f)
+  (lambda (i)
+    (f (f i))))
+
+(assert-equals 3 ((double inc) 1))
+(assert-equals 21 (((double (double double)) inc) 5))
+ 
