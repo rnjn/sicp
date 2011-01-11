e5cdb3f40c217b0d028f6499bdbd0dd6a76c4659
rnjn <rsakalle@thoughtworks.com>
2011-01-11 18:58:00 +0530

    interval math

---
+++ 2_7.scm
@@ -0,0 +1,26 @@
+(load "common.scm")
+
+(define (lower-bound x)
+  (car x))
+
+(define (upper-bound x)
+  (cdr x))
+
+(define (make-interval a b)
+  (cons a b))
+
+(define (add-interval x y)
+  (make-interval 
+   (+ (lower-bound x) (lower-bound y))
+   (+ (upper-bound x) (upper-bound y))))
+
+;tests
+(define a1 (make-interval 4.0 4.5))
+(define a2 (make-interval 3.9 4.1))
+(define a1-plus-a2 (add-interval a1 a2))
+
+(assert-equals 8 (lower-bound (make-interval 8 9)))
+(assert-equals 9 (upper-bound (make-interval 8 9)))
+
+(assert-equals 7.9 (lower-bound a1-plus-a2))
+(assert-equals 8.6 (upper-bound a1-plus-a2))
---
+++ 2_8.scm
@@ -0,0 +1,30 @@
+(load "common.scm")
+
+(define (lower-bound x)
+  (car x))
+
+(define (upper-bound x)
+  (cdr x))
+
+(define (make-interval a b)
+  (cons a b))
+
+(define (add-interval x y)
+  (make-interval 
+   (+ (lower-bound x) (lower-bound y))
+   (+ (upper-bound x) (upper-bound y))))
+
+;[a,b] − [c,d] = [a − d, b − c]
+(define (sub-interval x y)
+  (add-interval x
+   (make-interval 
+    (- (upper-bound y))
+    (- (lower-bound y)))))
+
+;tests - 
+(define a (make-interval 4 5))
+(define b (make-interval 1 2))
+(define a-sub-b (sub-interval a b))
+
+(assert-equals 2 (lower-bound a-sub-b))
+(assert-equals 4 (upper-bound a-sub-b))
