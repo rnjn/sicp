4071c77e552aad5601e189084a161f1fd37d28af
rnjn <rsakalle@thoughtworks.com>
2011-01-12 08:10:21 +0530

    fringes of a tree

---
+++ 2_28.scm
@@ -0,0 +1,17 @@
+(load "common.scm")
+
+(define (fringe l)
+  (define (iterate source result)
+    (cond ((null? source) result)
+	  ((not (pair? source)) (cons source result))
+	  (else
+	   (append 
+	    (iterate (car source) result)
+	    (iterate (cdr source) result)))))
+  (iterate l '()))
+
+
+(define x (list (list 1 2) (list 3 4)))
+
+(assert-equals '(1 2 3 4) (fringe x))
+(assert-equals '(1 2 3 4 1 2 3 4) (fringe (list x x)))
