86fdcb838320b113726568695b4bf6d06e307643
rnjn <rsakalle@thoughtworks.com>
2011-01-12 18:34:25 +0530

    accumulations used for list operations

---
+++ 2_33.scm
@@ -0,0 +1,30 @@
+(load "common.scm")
+
+(define (accumulate f initial sequence)
+  (if (null? sequence) initial
+      (f (car sequence)
+	 (accumulate f initial (cdr sequence)))))
+
+(define (map f sequence)
+  (accumulate (lambda (x y)
+		(cons (f x) (map f (cdr sequence)))) nil sequence))
+
+
+(define (append seq1 seq2)
+  (accumulate cons seq2 seq1))
+
+
+(define (length sequence)
+  (accumulate (lambda (x y)
+		(if (null? x) 
+		(+ 1 (length (cdr sequence))))
+	      0 sequence))
+
+;tests
+;map
+(define square (lambda (x) (* x x)))
+(assert-equals '(1 4 9 16) (map square (list 1 2 3 4)))
+;append
+(assert-equals '(1 2 3 4) (append (list 1 2) (list 3 4)))
+;length
+(assert-equals 3 (length (list 1 2 3)))
